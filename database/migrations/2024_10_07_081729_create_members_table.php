265baa141642cc7b0df7c22366d64d3d99ded173
Arifmehdi <mehediarif.du@gmail.com>
2024-10-07 15:11:13 +0600

    membership form submitted

--- database/migrations/2024_10_07_081729_create_members_table.php
+++ database/migrations/2024_10_07_081729_create_members_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('member_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('email');
+            $table->string('dba');
+            $table->text('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('company');
+            $table->string('store_phone');
+            $table->string('store_fax');
+            $table->string('cell_phone');
+            $table->string('cell_carrier');
+            $table->string('business_type');
+            $table->string('number_of_store');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
